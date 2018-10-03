b52e1484b8bcec284421f50756228fc8ce37b07d
blackopss <36933564+blackopss@users.noreply.github.com>
2018-10-03 19:02:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,4 +107,48 @@ const w = ['./no4.png'];
 })
 });
 
+
+
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("475821467393589248");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+
+
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("476154996216430598");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("475821467393589248");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    setTimeout(function() {
+ channel.send(`**invited by** ${Invite.inviter} `) ;
+                    },1500);
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
+
 client.login(process.env.BOT_TOKEN);
