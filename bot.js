a24d609a6b6180e2fbbdaca3c806b2182b751ff3
blackopss <36933564+blackopss@users.noreply.github.com>
2018-10-03 19:17:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,14 @@ const fs = require('fs');
 const moment = require('moment');
 const jimp = require('jimp');
 const Canvas = require('canvas');
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
 
+client.on('ready', () => {
+	console.log('I am Fucker!'); 
+  });
 
 client.on('guildMemberAdd', member => {
 const channel = member.guild.channels.find("name","chat")
@@ -106,49 +113,6 @@ const w = ['./no4.png'];
 
 
 
-var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
-}
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("475821467393589248");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        });
-    });
-});
 
-
-client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.get("476154996216430598");
-    if (!channel) {
-        console.log("!the channel id it's not correct");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('-');
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("475821467393589248");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    setTimeout(function() {
- channel.send(`**invited by** ${Invite.inviter} `) ;
-                    },1500);
- }
-            dat[Inv] = Invite.uses;
-       
-       });
-    });
-});
 
 client.login(process.env.BOT_TOKEN);
