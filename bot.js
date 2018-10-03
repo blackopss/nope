9954e3c3ef428de6a820de8e07f1b3703954b83a
blackopss <36933564+blackopss@users.noreply.github.com>
2018-10-03 20:09:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,15 +15,15 @@ client.on('ready', () => {
 
 
 
+
 client.on("message", (message) => {
     
-  if(message.content.startsWith('new')) {
+  if(message.content.startsWith('تذكره')) {
         const reason = message.content.split(" ").slice(1).join(" ");
-        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-        if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
-        message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
-            let role = message.guild.roles.find("name", "Support Team");
-            let role2 = message.guild.roles.find("name", "@everyone");
+        if (!message.guild.roles.exists("name", "Elite")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+        if (message.guild.channels.exists("name", "تذكره شراء" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
+        message.guild.createChannel(`تذكره شراء ${message.author.id}`, "text").then(c => {
+            let role = message.guild.roles.find("name", "Elite");
             c.overwritePermissions(role, {
                 SEND_MESSAGES: true,
                 READ_MESSAGES: true
@@ -36,10 +36,10 @@ client.on("message", (message) => {
                 SEND_MESSAGES: true,
                 READ_MESSAGES: true
             });
-            message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
+            message.channel.send(`تم فتح تذكره لك وشكرا , #${c.name}.`);
             const embed = new Discord.RichEmbed()
                 .setColor(0xCF40FA)
-                .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Staff** will be here soon to help.`)
+                .addField(`هاي ${message.author.username}!`, `شكرا لك على فتح تذكره شراء سيتم الرد عليك خلال دقائق وشكرا.`)
                 .setTimestamp();
             c.send({
                 embed: embed
@@ -47,9 +47,9 @@ client.on("message", (message) => {
         }).catch(console.error); 
     }
   if(message.content.startsWith('close')) {
-        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+        if (!message.channel.name.startsWith(`تذكره شرا`)) return message.channel.send(`اسف لكن لا يمكنك استخدام هذا الامر خارج رومك لشراء التذكره.`);
 
-        message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`c\`. This will time out in 10 seconds and be cancelled.`)
+        message.channel.send(` لديك عشر ثواني وشكرا لك /``c``/ هل انت متاكد؟, لو متاكد اكتب `)
             .then((m) => {
                 message.channel.awaitMessages(response => response.content === 'c', {
                         max: 1,
